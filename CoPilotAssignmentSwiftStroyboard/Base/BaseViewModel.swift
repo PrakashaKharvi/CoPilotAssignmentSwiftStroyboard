2e5c631760d90c371f61e65c9028036a8a0157ce
Prakasha Kharvi <prakasha@robosoftin.com>
2025-03-05 14:42:09 +0530

    Integrating MVVM with clean Architecture into App

---
+++ CoPilotAssignmentSwiftStroyboard/Base/BaseViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewModel.swift
+//  CoPilotAssignmentSwiftStroyboard
+//
+//  Created by Prakasha on 05/03/25.
+//
+
+import Foundation
+
+class BaseViewModel {
+    
+    init() {}
+    
+}
+
+
+
